2e8fab5cecd5362f82219dfda023e8e042abf911
Ian Kamau <kian99564@gmail.com>
2024-02-12 13:20:51 +0300

    config: setup jest test environment

--- src/__test__/components/Navbar.test.tsx
+++ src/__test__/components/Navbar.test.tsx
@@ -2,19 +2,6 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Navbar from "@/components/navbar/Navbar";
 
-// jest.mock("next/router", () => ({
-//   useRouter: jest.fn(),
-// }));
-
-// describe("Page", () => {
-//   it("renders a heading", () => {
-//     render(<Navbar />);
-
-//     const heading = screen.getByRole("link");
-//     expect(heading).toBeInTheDocument();
-//   });
-// });
-
 describe("sample test", () => {
   it("example test", () => {
     expect(1 + 1).toBe(2);
