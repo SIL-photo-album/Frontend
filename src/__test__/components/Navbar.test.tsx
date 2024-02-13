490c0d64c9e20bc7ebfb2734877c64e94c00f715
Ian Kamau <kian99564@gmail.com>
2024-02-13 15:18:42 +0300

    test: tested navar

--- src/__test__/components/Navbar.test.tsx
+++ src/__test__/components/Navbar.test.tsx
@@ -1,10 +1,18 @@
 import "@testing-library/jest-dom";
+import { useRouter } from "next/navigation";
 import { render, screen } from "@testing-library/react";
+import LoginButton from "@/components/button/LoginButton";
+
 import Navbar from "@/components/navbar/Navbar";
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}));
 
 describe("sample test", () => {
   it("example test", () => {
-    // render(<Navbar />)
-    expect(1 + 1).toBe(2);
+    render(<Navbar />);
+    expect(screen.getByText("Ian Kamau")).toBeInTheDocument();
+    expect(screen.getByText("Ian Kamau").getAttribute("href")).toBe("/");
   });
 });
